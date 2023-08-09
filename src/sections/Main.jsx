199797f7b65d0486ea86ee68e264ffd2ec3c390d
fettahaud <fettahaud@gmail.com>
2023-08-09 22:28:04 +0300

    added animation

--- src/sections/Main.jsx
+++ src/sections/Main.jsx
@@ -1,11 +1,54 @@
+import { gsap } from "gsap";
+import { useRef } from "react";
+import { useEffect } from "react";
+
 export default function Main() {
+  const titleRef = useRef();
+  const secondTitleRef = useRef();
+  const title = "My name is Fettah";
+  const secondTitle = "A Frontend/Creative Developer";
+  const tl = gsap.timeline();
+  useEffect(() => {
+    tl.fromTo(
+      titleRef.current.children,
+      {
+        opacity: 0,
+        duration: 1,
+        top: -20,
+      },
+      {
+        opacity: 1,
+        stagger: 0.05,
+        top: 0,
+      }
+    ).fromTo(
+      secondTitleRef.current.children,
+      {
+        opacity: 0,
+        duration: 1,
+        top: -15,
+      },
+      {
+        opacity: 1,
+        stagger: 0.05,
+        top: 0,
+      },
+      "-=1.2"
+    );
+  });
   return (
     <section id="main" className="w-full">
       <div className="title">
-        <h1 className="">
-          My name is <b>Fettah</b>
+        <h1 ref={titleRef}>
+          {[...title].map((e, i) => (
+            <span key={i}>{e}</span>
+          ))}
         </h1>
-        <h2 className="">A Frontend/Creative Developer</h2>
+        <h2 ref={secondTitleRef}>
+          {[...secondTitle].map((e, i) => (
+            <span key={i}>{e}</span>
+          ))}
+        </h2>
       </div>
     </section>
   );
