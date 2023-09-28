0c258c988bbd20f62b42a25f339ae7731006af8f
fettahaud <fettahaud@gmail.com>
2023-09-28 19:02:41 +0300

    flexibility to mouse scale

--- src/components/SubPageHeader.jsx
+++ src/components/SubPageHeader.jsx
@@ -1,18 +1,53 @@
 import { useLocation } from "react-router-dom";
+import { useHover } from "./Mouse.jsx";
+import { useEffect, useLayoutEffect, useRef } from "react";
 
 export default function SubPageHeader() {
   const { pathname } = useLocation();
+  const { setIsHovered } = useHover();
+
+  const hoverAbleEl = useRef([]);
+
+  useEffect(() => {
+    if (hoverAbleEl.current) {
+      hoverAbleEl.current.forEach((el) => {
+        el.addEventListener("mouseover", () =>
+          setIsHovered(el.dataset.mouseScale),
+        );
+        el.addEventListener("mouseout", () => setIsHovered(1));
+      });
+    }
+  }, []);
+
   return (
     <div className="subpage-header md:mx-auto md:container px-8">
       <div className="upper">
-        <span>Fettah /&nbsp;</span>
-        <span>
+        <span
+          ref={(el) => hoverAbleEl.current.push(el)}
+          className="hoverEffect"
+          data-mouse-scale={3}
+        >
+          Fettah /&nbsp;
+        </span>
+        <span
+          ref={(el) => hoverAbleEl.current.push(el)}
+          className="hoverEffect"
+          data-mouse-scale={3}
+        >
           {pathname
             .replace(/\//g, " ")
             .replace(/\b\w/g, (match) => match.toUpperCase())}
         </span>
       </div>
-      <div className="lower">Web Developer</div>
+      <div className="lower ">
+        <span
+          ref={(el) => hoverAbleEl.current.push(el)}
+          className="hoverEffect"
+          data-mouse-scale={3}
+        >
+          Web Developer
+        </span>
+      </div>
     </div>
   );
 }
