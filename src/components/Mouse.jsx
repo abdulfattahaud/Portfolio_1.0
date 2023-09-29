0cc6b3b4a2a7f8684796718632a8d0ed11e85719
fettahaud <fettahaud@gmail.com>
2023-09-29 18:05:47 +0300

    Fixed mouse with scroll

--- src/components/Mouse.jsx
+++ src/components/Mouse.jsx
@@ -24,29 +24,47 @@ export function HoverProvider({ children }) {
 }
 
 export default function Mouse() {
-  const mouseRef = useRef();
   const { hover } = useHover();
 
   const moveLight = () => {
-    const x = gsap.quickTo("#mouse", "left", {
-      duration: 0.1,
-    });
-    const y = gsap.quickTo("#mouse", "top", {
-      duration: 0.1,
-    });
+    const mouse = document.querySelector("#mouse");
+    let mouseX = 0;
+    let mouseY = 0;
+
+    const updateMousePosition = (e) => {
+      mouseX = e.clientX;
+      mouseY = e.clientY;
+      updateMouseElementPosition();
+    };
+
+    const updateMouseElementPosition = () => {
+      const x = mouseX + window.scrollX;
+      const y = mouseY + window.scrollY;
+
+      // Set the position of the "mouse" element
+      mouse.style.left = `${x}px`;
+      mouse.style.top = `${y}px`;
+    };
 
     const handleMouseMove = (e) => {
-      x(e.clientX);
-      y(e.clientY);
+      // Update mouse position when the mouse or touch moves
+      updateMousePosition(e);
+    };
+
+    const handleScroll = () => {
+      // Update mouse position when scrolling
+      updateMouseElementPosition();
     };
 
     window.addEventListener("mousemove", handleMouseMove);
     window.addEventListener("touchmove", handleMouseMove);
+    window.addEventListener("scroll", handleScroll);
 
     // Return a cleanup function to remove the event listeners when the component unmounts.
     return () => {
       window.removeEventListener("mousemove", handleMouseMove);
       window.removeEventListener("touchmove", handleMouseMove);
+      window.removeEventListener("scroll", handleScroll);
     };
   };
 
@@ -57,11 +75,5 @@ export default function Mouse() {
     };
   }, []);
 
-  return (
-    <div
-      ref={mouseRef}
-      id="mouse"
-      style={{ transform: `scale(${hover})` }}
-    ></div>
-  );
+  return <div id="mouse" style={{ transform: `scale(${hover})` }}></div>;
 }
