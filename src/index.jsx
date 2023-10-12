0654861cb98411785d739ddfba51d6086cda94c1
fettahaud <fettahaud@gmail.com>
2023-10-12 23:16:25 +0300

    delete old

--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-import "./index.scss";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import RouterApp from "./RouterApp.jsx";
-
-const root = ReactDOM.createRoot(document.querySelector("#root"));
-
-root.render(<RouterApp />);
--- src/store.js
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-
-const store = create((set) => ({
-  menuState: false,
-  changeMenuState: (data) => set((state) => ({ menuState: data })),
-}));
-
-export default store;
