1cd3912d9dc49135c63207348358fb708c242f29
fettahaud <fettahaud@gmail.com>
2023-10-15 20:28:18 +0300

    added some projects

--- src/app/assets/projects.ts
+++ src/app/assets/projects.ts
@@ -1,10 +1,32 @@
+enum ColorOption {
+  FADEC9 = "#FADEC9",
+  E8DEEE = "#E8DEEE",
+  DBEDDB = "#DBEDDB",
+  D3E5EF = "#D3E5EF",
+  EFE3D3 = "#EFE3D3",
+  EFD5D3 = "#EFD5D3",
+  EFEAD3 = "#EFEAD3",
+  FDECC8 = "#FDECC8",
+  F1F0EF = "#F1F0EF",
+  F5E0E9 = "#F5E0E9",
+  EEE0DA = "#EEE0DA",
+  DAC9FA = "#DAC9FA",
+}
+
+const colorOptions: ColorOption[] = Object.values(ColorOption);
+
+function getRandomColor(): ColorOption {
+  const randomIndex = Math.floor(Math.random() * colorOptions.length);
+  return colorOptions[randomIndex];
+}
+
 interface Project {
   id: number;
   title: string;
-  image: string;
-  description: string;
-  details: string;
-  tags: { label: string; color: string }[];
+  image?: string;
+  description?: string;
+  details?: string;
+  tags: { label: string; color: ColorOption }[];
   github: string;
   live: string;
 }
@@ -12,44 +34,95 @@ interface Project {
 const projectsArr: Project[] = [
   {
     id: 0,
-    title: "Project 1",
+    title: "2D Particles",
     image: "/images/project-1.png",
     description: "some description",
     details: "lorem ipusum for now ",
     tags: [
-      { label: "html", color: "#fdecc8" },
-      { label: "css", color: "#fdecc8" },
-      { label: "js", color: "#fdecc8" },
+      { label: "JS", color: getRandomColor() },
+      { label: "WebGl", color: getRandomColor() },
+      { label: "3Js", color: getRandomColor() },
     ],
-    github: "github.com/fettahaud/project1",
-    live: "project1.com",
+    github: "https://github.com/FettahAud/2D-Particles",
+    live: "https://2-d-particles.vercel.app/",
   },
   {
     id: 1,
-    title: "Project 2",
-    image: "/images/project-1.png",
-    description: "some description",
-    details: "lorem ipusum for now ",
+    title: "Travel Visualizer",
     tags: [
-      { label: "react", color: "#fdecc8" },
-      { label: "tailwind", color: "#fdecc8" },
+      { label: "JS", color: getRandomColor() },
+      { label: "GLSL", color: getRandomColor() },
+      { label: "R3f", color: getRandomColor() },
     ],
-    github: "github.com/fettahaud/project2",
-    live: "project2.com",
+    github: "https://github.com/FettahAud/travel-viualizer",
+    live: "https://travel-visualizer.vercel.app/",
   },
   {
     id: 2,
-    title: "Project 3",
-    image: "/images/project-1.png",
-    description: "some description",
-    details: "lorem ipusum for now ",
+    title: "Sphere Curl",
     tags: [
-      { label: "3js", color: "#fdecc8" },
-      { label: "r3f", color: "#fdecc8" },
-      { label: "gsap", color: "#fdecc8" },
+      { label: "R3F", color: getRandomColor() },
+      { label: "WebGl", color: getRandomColor() },
+      { label: "Fbo", color: getRandomColor() },
     ],
-    github: "github.com/fettahaud/project3",
-    live: "project3.com",
+    github: "https://github.com/FettahAud/curl-particles",
+    live: "https://sphere-curl.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Fbo Particles",
+    tags: [
+      { label: "R3F", color: getRandomColor() },
+      { label: "Fbo", color: getRandomColor() },
+      { label: "Shaders", color: getRandomColor() },
+      { label: "Js", color: getRandomColor() },
+    ],
+    github: "https://github.com/FettahAud/fbo-particles",
+    live: "https://fbo-particles-nine.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "Ping pong game",
+    tags: [
+      { label: "R3F", color: getRandomColor() },
+      { label: "Rapier", color: getRandomColor() },
+      { label: "Js", color: getRandomColor() },
+    ],
+    github: "https://github.com/FettahAud/pingpong",
+    live: "https://pingpong-silk.vercel.app",
+  },
+  {
+    id: 5,
+    title: "Hover Animation",
+    tags: [
+      { label: "HTML", color: getRandomColor() },
+      { label: "Scss", color: getRandomColor() },
+      { label: "Js", color: getRandomColor() },
+      { label: "Animations", color: getRandomColor() },
+    ],
+    github: "https://github.com/FettahAud/hover-animation",
+    live: "https://hover-animation.vercel.app/",
+  },
+  {
+    id: 6,
+    title: "Fox Animations",
+    tags: [
+      { label: "3Js", color: getRandomColor() },
+      { label: "Gsap", color: getRandomColor() },
+      { label: "Animations", color: getRandomColor() },
+    ],
+    github: "https://github.com/FettahAud/Walking-Fox",
+    live: "https://moving-fox.vercel.app/",
+  },
+  {
+    id: 7,
+    title: "Physics animation",
+    tags: [
+      { label: "R3F", color: getRandomColor() },
+      { label: "Rapier", color: getRandomColor() },
+    ],
+    github: "https://github.com/FettahAud/rapier-test",
+    live: "https://rapier-test.vercel.app/",
   },
 ];
 
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -14,9 +14,6 @@ export default function Header() {
       }
     };
     window.addEventListener("scroll", scroll);
-    return () => {
-      window.removeEventListener("scroll", scroll);
-    };
   }, []);
   return (
     <header ref={header} className="header">
