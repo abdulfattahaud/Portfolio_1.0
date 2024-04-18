f3a8e5ec198179d779cc1e9e495653222d4eaf0c
Abdülfettah Adwani <fettahaud@gmail.com>
2024-04-18 12:16:51 +0300

    added icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ export const metadata: Metadata = {
   title: "Fettah",
   description:
     "Frontend / Creative Developer. I&apos;ll make your design come alive",
+  // Add favicon
 };
 
 export default function RootLayout({
@@ -17,6 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logo.png" type="image/png" sizes="32x32" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
