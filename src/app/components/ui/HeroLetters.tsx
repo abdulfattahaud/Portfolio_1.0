bfa34267e18cbf93c6838953319a7c96db10937f
Abdülfettah Adwani <fettahaud@gmail.com>
2024-05-21 16:20:17 +0300

    change letters

--- src/app/components/ui/HeroLetters.tsx
+++ src/app/components/ui/HeroLetters.tsx
@@ -1,10 +1,8 @@
 const ALetter = () => {
   return (
-    <svg viewBox="0 0 255 234" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg viewBox="0 0 256 233" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
-        d="M96.1543 0.452393L0.0205078 233.332H55.5979L75.8761 182.164H75.8942L91.2748 142.141H91.2755L127.323 49.9804L163.748 142.148H163.745L179.155 182.174H179.149L199.423 233.332H255L158.867 0.452393H96.1543Z"
+        d="M96.8701 0L0.736328 232.88H56.3137L76.5919 181.712H76.61L91.9906 141.688L128.038 49.528L164.464 141.696H164.461L179.871 181.722H179.865L200.139 232.88H255.716L159.582 0H96.8701Z"
         fill="white"
       />
     </svg>
@@ -14,10 +12,9 @@ const ALetter = () => {
 const FLetter = () => {
   return (
     <svg viewBox="0 0 183 234" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M182.518 40.7263V0.109838L0 0V40.7263H182.518Z" fill="white" />
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
-        d="M182.504 0.266357H0V40.9039H55.1946V40.9384H182.504V0.266357ZM54.8263 99.2966H0V233.146H54.8263V139.338H85.0559H115.285H175.745V99.3224H115.285H85.0559H54.8263V99.2966Z"
+        d="M0.0135318 96.3007H175.305V136.155H54.5394V233.081H0.0135318V96.3007Z"
         fill="white"
       />
     </svg>
@@ -25,11 +22,17 @@ const FLetter = () => {
 };
 const ELetter = () => {
   return (
-    <svg viewBox="0 0 187 233" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg viewBox="0 0 188 233" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
-        d="M0 0H183.631V40.672H54.8263V40.6744H0V0ZM0 96.1063V136.107H60.4824V136.12H179.124V96.104H61.5907V96.1063H0ZM60.4824 192.304V192.208H186.635V232.88H0V192.304H60.4824Z"
+        d="M0.503906 0H184.134V40.672H55.3302V40.6744H0.503906V0Z"
+        fill="white"
+      />
+      <path
+        d="M0.503906 96.1063V136.107H60.9863V136.12H179.628V96.104H62.0946V96.1063H0.503906Z"
+        fill="white"
+      />
+      <path
+        d="M60.9863 192.304V192.208H187.139V232.88H0.503906V192.304H60.9863Z"
         fill="white"
       />
     </svg>
@@ -37,11 +40,14 @@ const ELetter = () => {
 };
 const HLetter = () => {
   return (
-    <svg viewBox="0 0 212 234" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg viewBox="0 0 213 233" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
-        d="M0.205078 94.4486H55.0314V0.366089H0.205078V94.4486ZM110.34 95.8141V136.486H157.174V233.246H212V0.366089H157.174V95.8141H110.34Z"
+        d="M0.716797 0V232.88H55.5431V140.262H55.5404V92.6183H55.5431V0H0.716797Z"
+        fill="white"
+      />
+      <path d="M68.4144 95.448V136.12H144.787V95.448H68.4144Z" fill="white" />
+      <path
+        d="M157.685 136.12H157.661V95.448H157.685V0H212.512V232.88H157.685V136.12Z"
         fill="white"
       />
     </svg>
@@ -49,13 +55,12 @@ const HLetter = () => {
 };
 const TLetter = () => {
   return (
-    <svg viewBox="0 0 216 233" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg viewBox="0 0 215 233" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
-        fill-rule="evenodd"
-        clip-rule="evenodd"
-        d="M215.43 40.6923V0.0202637H0.630859V40.6923H80.6172V40.6984H135.443V40.6923H215.43ZM135.443 85H80.6172V232.9H135.443V85Z"
+        d="M214.938 40.672V0H0.138672V40.672H79.5205V40.6704L135.054 40.6704V40.672H214.938Z"
         fill="white"
       />
+      <path d="M134.951 96.0271H80.125V232.88H134.951V96.0271Z" fill="white" />
     </svg>
   );
 };
