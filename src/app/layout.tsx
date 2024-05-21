016acbd40b0116b51d89784a6a27c08f1ec82c38
Abdülfettah Adwani <fettahaud@gmail.com>
2024-05-21 11:12:35 +0300

    fix fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,10 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/logo.png" type="image/png" sizes="32x32" />
+        <link
+          href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,regular,500,600,700,800,900,100italic,200italic,300italic,italic,500italic,600italic,700italic,800italic,900italic"
+          rel="stylesheet"
+        />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
