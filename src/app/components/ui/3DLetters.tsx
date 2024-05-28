42b1f8e7841e428b7ec8980c6379ab8f43588aa8
Abdülfettah Adwani <fettahaud@gmail.com>
2024-05-28 12:25:37 +0300

    added logo

--- src/app/components/ui/3DLetters.tsx
+++ src/app/components/ui/3DLetters.tsx
@@ -2,22 +2,35 @@ import { Svg } from "@react-three/drei";
 import { useThree } from "@react-three/fiber";
 // import { useScroll, useSpring, useTransform } from "framer-motion";
 import { motion } from "framer-motion-3d";
+import { useEffect, useState } from "react";
 // import { useEffect } from "react";
 
 // import { useControls } from "leva";
 
 export default function TDLetters() {
   const { viewport } = useThree();
+  const [scale, setScale] = useState(viewport.width / 7.6);
+  const updateScale = () => {
+    if (document.documentElement.clientWidth < 768) {
+      setScale(0.6);
+    } else {
+      setScale(viewport.width / 7.6);
+    }
+  };
+  useEffect(() => {
+    updateScale();
+    window.addEventListener("resize", updateScale);
+    return () => {
+      window.removeEventListener("resize", updateScale);
+    };
+  }, []);
   // const { x } = useControls("3D Letters", { x: { value: 0, step: 0.1 } });
 
   // const { scrollYProgress } = useScroll();
   // const y = useTransform(scrollYProgress, [0, 0.25], [1, 0]);
   // const springY = useSpring(y, { stiffness: 400, damping: 90 });
   return (
-    <motion.group
-      scale={viewport.width / 7.6}
-      position={[0, viewport.height / 2 - 1, -2]}
-    >
+    <motion.group scale={scale} position={[0, viewport.height / 2 - 1, -2]}>
       {/* <motion.group position={[0, springY, 0]}> */}
       <Svg src="/FLetter.svg" scale={0.01} position={[-5.3, 0, 0]} />
       {/* </motion.group> */}
--- src/app/components/ui/3Donut.tsx
+++ src/app/components/ui/3Donut.tsx
@@ -1,30 +1,48 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { MeshTransmissionMaterial, useGLTF } from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
 import { useScroll, useSpring, useTransform } from "framer-motion";
 import { motion } from "framer-motion-3d";
+import * as THREE from "three";
 
 export default function Donut() {
   const { viewport } = useThree();
-  const torus = useRef(null);
+  const torus = useRef<THREE.Mesh>(null);
+  const [scale, setScale] = useState(0.3);
   const { scrollYProgress } = useScroll({
     //@ts-ignore
     container: ".hero",
   });
   const y = useTransform(scrollYProgress, [0, 1], [0, 2.5]);
   const springY = useSpring(y, { stiffness: 400, damping: 90 });
-  useFrame(() => {
-    //@ts-ignore
-    torus.current.rotation.x += 0.02;
-    //@ts-ignore
-    torus.current.rotation.z -= 0.02;
+  useFrame(({ clock }) => {
+    if (!torus.current) return;
+    torus.current.rotation.y = clock.getElapsedTime();
+    torus.current.rotation.x = clock.getElapsedTime();
+    // torus.current.position.y = Math.sin(clock.getElapsedTime()) * 0.1;
+    // torus.current.position.x = Math.sin(clock.getElapsedTime());
   });
+  const updateScale = () => {
+    if (document.documentElement.clientWidth < 768) {
+      setScale(0.6);
+    } else {
+      setScale(0.3);
+    }
+  };
+  useEffect(() => {
+    updateScale();
+    window.addEventListener("resize", updateScale);
+    return () => {
+      window.removeEventListener("resize", updateScale);
+    };
+  }, []);
 
   return (
     <group scale={viewport.width / 3.75}>
       {/* @ts-ignore */}
-      <motion.mesh ref={torus} scale={0.3} position={[0, springY, 0]}>
-        <torusKnotGeometry args={[1, 0.4, 100, 32]} />
+      <motion.mesh ref={torus} scale={scale} position={[0, springY, 0]}>
+        {/* <torusKnotGeometry args={[1, 0.4, 100, 32]} /> */}
+        <torusGeometry args={[1, 0.4, 100, 32]} />
         {/* @ts-ignore */}
         <MeshTransmissionMaterial
           thickness={0.2}
